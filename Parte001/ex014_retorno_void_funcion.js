900cdbfa370356ca0a32f12e1e127c5b96311c22
infzer0 <johnortizo@outlook.com>
2020-01-25 14:03:03 -0500

    TypeScript - Ejercicio 14
    TypeScript - Ejercicio 14: Uso de void para Indicar que una Función no Retorna Ningún Valor.

---
+++ Parte001/ex014_retorno_void_funcion.js
@@ -0,0 +1,5 @@
+// Ejercicio 14: Uso de void para indicar que una función no realiza ningún retorno de datos.
+function mostrarMensaje(texto) {
+    console.log(texto);
+}
+console.log(mostrarMensaje('¡TypeScript es tremendo!'));
---
+++ Parte001/ex014_retorno_void_funcion.ts
@@ -0,0 +1,7 @@
+// Ejercicio 14: Uso de void para indicar que una función no realiza ningún retorno de datos.
+
+function mostrarMensaje(texto:string): void {
+    console.log(texto);
+}
+
+console.log(mostrarMensaje('¡TypeScript es tremendo!'));
