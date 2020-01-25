75ef2f760d4184aee1736d690c5fcf05d364e443
infzer0 <johnortizo@outlook.com>
2020-01-25 14:24:32 -0500

    TypeScript - Ejercicio 15
    TypeScript - Ejercicio 15: Uso de los Tipos null y undefined.

---
+++ Parte001/ex015_null_y_undefined.js
@@ -0,0 +1,5 @@
+// Ejercicio 15: Uso de los tipos null y undefined.
+let u = undefined;
+let n = null;
+console.log(u == undefined);
+console.log(n == null);
---
+++ Parte001/ex015_null_y_undefined.ts
@@ -0,0 +1,7 @@
+// Ejercicio 15: Uso de los tipos null y undefined.
+
+let u: undefined = undefined;
+let n: null = null;
+
+console.log(u == undefined);
+console.log(n == null);
